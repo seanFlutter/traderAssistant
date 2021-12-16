36e916d10587dec775caa7a6af7d1ff55c41e77d
seanFlutter <seanonuoha@gmail.com>
2021-12-16 02:57:23 +0100

    first refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,402 +2,44 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traderassistant/providers/market_provider.dart';
+import 'package:traderassistant/ui/notice_screen.dart';
 
 import 'Favorite.dart';
-import 'Market.dart';
+import 'ui/market_screen.dart';
 import 'PortFolios.dart';
 import 'RemoveAds.dart';
+import 'package:provider/provider.dart';
 import 'TargetPrice.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    /// Providers are above [MyApp] instead of inside it, so that tests
+    /// can use [MyApp] while mocking the providers
+    MultiProvider(
+      providers: [
+       ChangeNotifierProvider(create: (_) => MarketProvider()),
+      ],
+      child:  MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,home: FirstPage());
+    return MaterialApp(debugShowCheckedModeBanner: false,initialRoute: NoticeScreen.id, routes: {
+NoticeScreen.id : (context) => NoticeScreen(),
+MarketScreen.id : (context) => MarketScreen(),
+    },);
   }
 }
 ////FirstPage
-class FirstPage extends StatefulWidget {
-  const FirstPage({Key? key}) : super(key: key);
-
-  @override
-  _FirstPageState createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Color(0xff404040),
-        appBar: AppBar(
-          elevation: 0,
-          toolbarHeight: 50,
-          bottom: TabBar(
-              tabs:<Widget>[
-
-                Text('NOTICES'),
-                Text('ARCHIVE'),
-
-              ]
-          ),
-          backgroundColor: Color(0xff404040),
-          title: Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Trader Assistant',
-                    style: TextStyle(fontSize: 19, color: Colors.white),
-                  ),
-                  SizedBox(width: 8),
-                  Icon(Icons.search, size: 35),
-                  SizedBox(width: 8),
-                  Icon(
-                    Icons.archive,
-                    size: 35,
-                  ),
-                  SizedBox(width: 8),
-                  Icon(
-                    Icons.delete,
-                    size: 35,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(children: <Widget>[
-          Column(
-            children: [
-              
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text('10/12/21  17:16',style:TextStyle(color: Colors.white),),
-                  Text('EURUSD Cross Up Price 1.13129',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-                  Text('Cur:1.13129 High:1.13137 Target price reached.',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-                  Divider(thickness: 1,color: Colors.grey),
-                  Text('10/12/21  17:16',style:TextStyle(color: Colors.white),),
-                  Text('EURUSD Cross Up Price 1.13129',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal),),
-                  Text('Cur:1.13129 High:1.13137 Target price reached.',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal),),
-                  Divider(thickness: 1 ,color: Colors.grey),
-                  Text('10/12/21  17:16',style:TextStyle(color: Colors.white),),
-                  Text('The application has successfully connected to the server and ready to work ,You can go to the menu Markets to set a new signals and favorites.',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal),),
-                  Text('Cur:1.13129 High:1.13137 Target price reached.',style:TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal),),
-                  Divider(thickness: 1 ,color: Colors.grey),
-
-                ],
-              ),
-            ],
-          ),
-          Text(''),
-        ],),
-        drawer: Drawer(
-
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                margin: EdgeInsets.all(0),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/stock2.jpeg'),
-                    fit: BoxFit.fitWidth,
-                  ),
-                  color: Color(0xff404040),
-                ),
-                child: Text(
-                  '',
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                ),
-              ),
-              ListTile(
-                  tileColor: Color(0xff404040),
-                  title: Row(
-                    children: [
-                      Icon(
-                        Icons.message,
-                        color: Colors.grey,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Notices',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Color(0xff676767),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context)=>const FirstPage(),),
-                    );
-                  }),
-              ListTile(
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.add_chart,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Text(
-                        'Target Price',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                tileColor: Color(0xff404040),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context)=>const TargetPrice(),),
-                  );
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                tileColor: Color(0xff404040),
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.add_shopping_cart_outlined,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Markets',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context)=>const Market(),),
-                  );
-                  // Update the state of the app.
-                  // ...
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.add_to_queue_rounded,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Text(
-                        'Favorites',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                tileColor: Color(0xff404040),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context)=>const Favorites(),),
-                  );
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                tileColor: Color(0xff404040),
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.ad_units_sharp,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'PortFolios',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context)=>const PortFolios(),),
-                  );
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.cancel,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: const Text(
-                        'Remove Ads',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                tileColor: Color(0xff404040),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context)=>const RemoveAds(),),
-                  );
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              Divider(
-                color: Colors.white24,
-                height: 0.5,
-              ),
-              ListTile(
-                tileColor: Color(0xff404040),
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.settings,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Settings',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                tileColor: Color(0xff404040),
-                title: Row(
-                  children: [
-                    Icon(
-                      Icons.help,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Help',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                tileColor: Color(0xff404040),
-                title: Row(
-                  children: [
-                    Icon(
-                      CupertinoIcons.stopwatch,
-                      color: Colors.grey,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'privacy',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xff676767),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              Container(
-                color: Color(0xff404040),
-                height: 50,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 
 
-class ListTitle extends StatelessWidget {
-  const ListTitle({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+
 
 
 
